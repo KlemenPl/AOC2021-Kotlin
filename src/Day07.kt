536952f2eb8fbf697ef6fd63d298964c1ced58eb
klemen <plestenjak.klemen@gmail.com>
2021-12-07 20:24:25 +0100

    Day 07 — final

--- src/Day07.kt
+++ src/Day07.kt
@@ -8,27 +8,18 @@ fun main() {
     /****************
      * Part 1:
      ***************/
-    var min = Int.MAX_VALUE
-    val max = input.maxOrNull()
-    for (i in 0..max!!) {
-        var fuel = 0
-        input.map { fuel += abs(it - i) }
-        if (fuel < min)
-            min = fuel
-    }
-    println("Min fuel needed: $min")
-
     val median =  if (input.size % 2 == 0)
         ((input[input.size / 2] + input[input.size / 2 - 1]) / 2)
      else
         (input[input.size / 2])
-    println("Min fuel needed (n logn + n): ${input.sumOf { abs(it - median) }}")
+    println("Min fuel needed: ${input.sumOf { abs(it - median) }}")
 
     /****************
      * Part 2:
      ***************/
-    min = Int.MAX_VALUE
-    for (i in 0..max) {
+    var min = Int.MAX_VALUE
+    val max = input.maxOrNull()
+    for (i in 0..max!!) {
         val to_pos = i
         var fuel = 0
         input.map {
