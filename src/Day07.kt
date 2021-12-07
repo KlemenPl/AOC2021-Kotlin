a3151b313178796cbc122d05d8de73e1c4f3e22a
klemen <plestenjak.klemen@gmail.com>
2021-12-07 11:21:09 +0100

    Day 07 — Optimized Part 1

--- src/Day07.kt
+++ src/Day07.kt
@@ -2,27 +2,33 @@ import kotlin.math.abs
 
 fun main() {
     val lines = get_input(7)
-    val input = lines[0].split(",").map { it.toInt() }
+    val input = lines[0].split(",").map { it.toInt() }.sorted()
 
 
     /****************
      * Part 1:
      ***************/
     var min = Int.MAX_VALUE
-    for (i in 1..input[0]) {
-        val to_pos = i
+    val max = input.maxOrNull()
+    for (i in 0..max!!) {
         var fuel = 0
-        input.map { fuel += abs(it - to_pos) }
+        input.map { fuel += abs(it - i) }
         if (fuel < min)
             min = fuel
     }
     println("Min fuel needed: $min")
 
+    val median =  if (input.size % 2 == 0)
+        ((input[input.size / 2] + input[input.size / 2 - 1]) / 2)
+     else
+        (input[input.size / 2])
+    println("Min fuel needed (n logn + n): ${input.sumOf { abs(it - median) }}")
+
     /****************
      * Part 2:
      ***************/
     min = Int.MAX_VALUE
-    for (i in 1..input[0]) {
+    for (i in 0..max) {
         val to_pos = i
         var fuel = 0
         input.map {
